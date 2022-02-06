2dc20ee926c43ed50fa332a28168601ea253a9b4
Luis Mayta <luis@hadenlabs.com>
2022-02-06 05:45:56 -0500

    refactor: generator plop (#3)

--- provision/generators/plop/generators/test.ts
+++ provision/generators/plop/generators/test.ts
@@ -23,10 +23,13 @@ export const testGenerator: PlopGeneratorConfig = {
   ],
   actions: (data) => {
     const answers = data as Answers
-    const containerPath = `${testPath}/replace_name-${slugify(answers.testName, '-')}`
+    const containerPath = `${testPath}/dash-${slugify(answers.testName, '-')}`
+    const containerDocsPath = `${containerPath}/docs`
 
     if (!pathExists(containerPath)) {
       pathMake(containerPath)
+      pathMake(containerDocsPath)
+      pathMake(path.join(containerDocsPath, 'include'))
     }
 
     const actions: Actions = []
@@ -34,7 +37,7 @@ export const testGenerator: PlopGeneratorConfig = {
     actions.push({
       type: 'add',
       templateFile: `${baseTemplatesPath}/test.add.hbs`,
-      path: `${testPath}/replace_name_${slugify(answers.testName, '_')}_test.go`,
+      path: `${testPath}/dash_${slugify(answers.testName, '_')}_test.go`,
       abortOnFail: true
     })
 
@@ -42,6 +45,13 @@ export const testGenerator: PlopGeneratorConfig = {
       type: 'add',
       templateFile: `${baseTemplatesPath}/test/main.add.hbs`,
       path: `${containerPath}/main.tf`,
+      abortOnFail: false
+    })
+
+    actions.push({
+      type: 'add',
+      templateFile: `${baseTemplatesPath}/test/data.add.hbs`,
+      path: `${containerPath}/data.tf`,
       abortOnFail: false
     })
 
@@ -63,6 +73,13 @@ export const testGenerator: PlopGeneratorConfig = {
       type: 'add',
       templateFile: `${baseTemplatesPath}/test/versions.add.hbs`,
       path: `${containerPath}/versions.tf`,
+      abortOnFail: true
+    })
+
+    actions.push({
+      type: 'add',
+      templateFile: `${baseTemplatesPath}/test/docs/include/terraform.md`,
+      path: `${containerDocsPath}/include/terraform.md`,
       abortOnFail: true
     })
 
