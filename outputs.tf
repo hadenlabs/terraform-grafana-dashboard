18dbf583ca84835e1aee198186efe998e966316c
Luis Mayta <luis@hadenlabs.com>
2022-02-06 05:25:08 -0500

    chore: implement make dashboard grafana (#3)

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "enabled" {
+  description = "Enabled property of module"
+  value       = local.outputs.enabled
+}
+
+output "id" {
+  description = "Grafana dashboard ID"
+  value       = local.outputs.enabled ? join("", grafana_dashboard.this.*.id) : ""
+}
+
+output "slug" {
+  description = "Grafana dashboard SLUG"
+  value       = local.outputs.enabled ? join("", grafana_dashboard.this.*.slug) : ""
+}
+
+output "uid" {
+  description = "Grafana dashboard UID"
+  value       = local.outputs.enabled ? join("", grafana_dashboard.this.*.uid) : ""
+}
+
+output "version" {
+  description = "Grafana dashboard version"
+  value       = local.outputs.enabled ? join("", grafana_dashboard.this.*.version) : ""
+}
